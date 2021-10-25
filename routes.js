c30e84a2c9d0297f9bf3f5fc46be7830692ed950
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-25 19:57:03 -0300

    get juego usuario funcionando

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { userInfo } = require('os');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -7,6 +8,25 @@ router.get('/', (req, res) => {
   res.send(html)
 })
 
+router.get('/abracadabra/usuarios', (req, res) => {
+  const user = fs.readFileSync('usuarios.json', 'utf-8')
+  res.send(user)
+})
 
+router.use('/abracadabra/juego/:usuario', (req,res,next) => {
+  const nombre = req.params.usuario;
+  const user = JSON.parse(fs.readFileSync('usuarios.json', 'utf-8'))
+  const usuario = user.usuarios.find((elemento) => {
+    return elemento == nombre;
+  }) 
+
+  usuario == undefined
+    ? res.sendFile(__dirname + '/assets/img/who.jpeg')
+    : next()
+})
+router.get('/abracadabra/juego/:usuario', (req,res) => {
+  const { usuario } = req.params;
+  res.send(`Usuario ${usuario} existe`)
+})
 
 module.exports = router;
